1ad651508f5a142feeab5aab405661f332fb2cac
milan3991 <m.mircic93@gmail.com>
2025-08-25 15:56:38 +0200

    bcg

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     return (
         <div className='home_wrapper'>
             <div className="home_content">
-            <div className="left_side">
+            <div className="home_left_side">
                 <div className="logo">
                     <img className="logoimg" src={logo} alt="Logo" />
                     <h1 className="logoheading">TROPIC SOLUTION</h1>
@@ -16,7 +16,7 @@ const Home = () => {
                 <h5 className="homesubheading">Elevating tech talents into visionary leaders through innovative training and real-world experience.</h5>
                 <GlobalButton text="Start learning" className='home_button' />
             </div>
-            <div className="right_side">
+            <div className="home_right_side">
                 <img className="homeimg" src={homepageimg} alt="homepageimg" />
             </div>
             </div>
